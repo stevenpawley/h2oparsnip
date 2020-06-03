b05b954ff3d119f85659da68144175ac2bbd8b38
Steven Pawley <dr.stevenpawley@gmail.com>
2020-06-02 23:27:30 -0600

    test save/load

---
+++ tests/testthat/test_save.R
@@ -0,0 +1,36 @@
+library(testthat)
+library(parsnip)
+library(tibble)
+library(magrittr)
+library(h2o)
+
+h2o.init(nthreads = 1)
+iris_df <- as_tibble(iris)
+
+test_that("model persistence, model spec", {
+  clf <-
+    mlp(mode = "classification") %>%
+    set_engine("h2o", seed = 1234)
+
+  fitted_clf <- clf %>% fit(Species ~., iris_df)
+  preds <- predict(fitted_clf, iris_df)
+
+  # save model
+  model_file <- tempfile(fileext = ".mod")
+  parsnip_file <- tempfile(fileext = ".rds")
+
+  saveRDS(fitted_clf, parsnip_file)
+  write_h2o(fitted_clf, model_file)
+
+  # remove model
+  h2o.removeAll()
+  remove(fitted_clf)
+
+  # restore model
+  restored_clf <- readRDS(parsnip_file)
+  restored_clf <- read_h2o(restored_clf, model_file)
+  preds_post <- predict(restored_clf, iris_df)
+
+  # compare
+  expect_equal(preds, preds_post)
+})
